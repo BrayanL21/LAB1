3f65cf4d101fda8aa98381083aba3cb2be985330
Brayan <brayanleal21@hotmail.com>
2022-08-30 06:49:11 -0600

    Lab

---
+++ Persona.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace arbol_ito
+{
+    public class Persona
+    {
+        public string name { get; set; }
+        public string dpi { get; set; }
+        public DateTime dateBirth { get; set; }
+        public string address { get; set; }
+
+        public Persona left = null, right = null;
+
+        public List<Persona> buscar(string nombre, List<Persona> lista, string dpi = "", Persona persona = null, Persona padre = null, int opcion = 0, int lado = 0) {
+            if(String.Compare(nombre, this.name) > 0){
+                if (string.IsNullOrEmpty(dpi))
+                {
+                    lista = this.right?.buscar(nombre, lista) ?? lista;
+                }
+                else
+                {
+                    lista = this.right?.buscar(nombre, lista, dpi, persona, this, opcion, 2) ?? lista;
+                }
+            }
+            else if(nombre == this.name){
+                if (string.IsNullOrEmpty(dpi)){
+                    lista.Add(this);
+                    lista = this.left?.buscar(nombre, lista, dpi, persona, this, opcion, 1) ?? lista;
+                    lista = this.right?.buscar(nombre, lista, dpi, persona, this, opcion, 2) ?? lista;
+                } else {
+                    if(dpi == this.dpi)
+                    {
+                        if (opcion == 0)
+                        {
+                            lista.Add(this);
+                        }
+                        else if (opcion == 1)
+                        {
+                            this.dateBirth = persona.dateBirth;
+                            this.address = persona.address;
+                        }
+                        else if (opcion == 2) {
+                            if (lado == 1)
+                            {
+                                if (this.left == null) {
+                                    if (this.right == null) {
+                                        padre.left = null;
+                                    }
+                                    else {
+                                        padre.left = this.right;
+                                    }
+                                }
+                                else {
+                                    if (this.right == null) {
+                                        padre.left = this.left;
+                                    }
+                                    else {
+                                        padre.left = this.left;
+                                        padre.left.insertar(this.right);
+                                    }
+                                }
+                            }
+                            else if (lado == 2)
+                            {
+                                if (this.left == null)
+                                {
+                                    if (this.right == null)
+                                    {
+                                        padre.right = null;
+                                    }
+                                    else
+                                    {
+                                        padre.right = this.right;
+                                    }
+                                }
+                                else
+                                {
+                                    if (this.right == null)
+                                    {
+                                        padre.right = this.left;
+                                    }
+                                    else
+                                    {
+                                        padre.right = this.left;
+                                        padre.right.insertar(this.right);
+                                    }
+                                }
+                            }
+                        }
+                    }else if(String.Compare(dpi, this.dpi) < 0)
+                    {
+                        lista = this.left?.buscar(nombre, lista, dpi, persona, this, opcion, 1) ?? lista;
+                    }else if(String.Compare(dpi, this.dpi) > 0)
+                    {
+                        lista = this.right?.buscar(nombre, lista, dpi, persona, this, opcion, 2) ?? lista;
+                    }
+                }
+            }else if(String.Compare(nombre, this.name) < 0){
+                if (string.IsNullOrEmpty(dpi))
+                {
+                    lista = this.left?.buscar(nombre, lista) ?? lista;
+                }
+                else {
+                    lista = this.left?.buscar(nombre, lista, dpi, persona, this, opcion, 1) ?? lista;
+                }
+            }            
+            return lista;
+        }
+
+        public void insertar(Persona tmpPersona)
+        {
+            Persona tmp = this;
+            if ((String.Compare(tmp.name, tmpPersona.name) > 0) || (tmpPersona.name == tmp.name && String.Compare(tmp.dpi, tmpPersona.dpi) > 0))
+            {
+                if (tmp.left != null)
+                {
+                    tmp.left.insertar(tmpPersona);
+                }
+                else
+                {
+                    tmp.left = tmpPersona;
+                }
+            }
+            else if ((String.Compare(tmp.name, tmpPersona.name) < 0) || (tmpPersona.name == tmp.name && String.Compare(tmp.dpi, tmpPersona.dpi) < 0))
+            {
+                if (tmp.right != null)
+                {
+                    tmp.right.insertar(tmpPersona);
+                }
+                else
+                {
+                    tmp.right = tmpPersona;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace arbol_ito
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var direccionArchivo = "C:\\Users\\digif\\Downloads\\input.csv";
+            Console.WriteLine("Ingrese la direccion del archivo.");
+            direccionArchivo = Console.ReadLine();
+            Persona raiz = null, tmpPersona = null;
+            using (StreamReader reader = new StreamReader(direccionArchivo)) {
+                var texto = reader.ReadToEnd();
+                List<Persona> personas = new List<Persona>();
+                if (!string.IsNullOrEmpty(texto)) {
+                    string[] linea;
+                    foreach (var item in texto.Split('\n')) {
+                        linea = item.Split(';');
+                        if (linea.Length == 2) {
+                            tmpPersona = JsonConvert.DeserializeObject<Persona>(linea[1]);
+                            switch (linea[0]) {
+                                case "INSERT":
+                                    if (raiz == null) {
+                                        raiz = tmpPersona;
+                                    }
+                                    else {
+                                        raiz.insertar(tmpPersona);
+                                    }
+                                    
+                                    break;
+                                case "PATCH":
+                                    if(raiz != null)
+                                    {
+                                        personas = raiz.buscar(tmpPersona.name, personas, tmpPersona.dpi, tmpPersona, null, 1, 0);
+                                    }
+                                    break;
+                                case "DELETE":
+                                    if (raiz != null) {
+                                        personas = raiz.buscar(tmpPersona.name, personas, tmpPersona.dpi, tmpPersona, null, 2, 0);
+                                    }
+                                    break;
+                            }
+                        }
+                    }
+                }
+                int opcion = 0;
+                do
+                {
+                    Console.WriteLine("Ingrese una opcion");
+                    Console.WriteLine("1. Buscar");
+                    Console.WriteLine("0. Salir");
+                    opcion = Convert.ToInt32(Console.ReadLine());
+
+                    switch (opcion)
+                    {
+                        case 1:
+                            if (raiz != null) {
+                                Console.WriteLine("Ingrese el nombre a buscar:");
+                                var nombre = Console.ReadLine();
+                                List<Persona> res = new List<Persona> ();
+                                res = raiz.buscar(nombre, res);
+                                foreach (var item in res) {
+                                    Console.WriteLine("{ \"name\":\"" + item.name + "\", \"dpi\":\"" + item.dpi + "\", \"dateBirth\":\"" + item.dateBirth + "\", \"address\":\"" + item.address + "\" }");
+                                }
+                            }    
+                            break;
+                    }
+                } while (opcion != 0);
+            }
+        }
+
+        
+    }
+}
